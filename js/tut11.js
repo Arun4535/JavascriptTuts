63e4a330513c360cd852ddc6e9380905e2c4ecfb
Arun4535 <arunkhanchandani05@gmail.com>
2021-08-16 13:09:51 +0530

    window object uses

---
+++ js/tut11.js
@@ -0,0 +1,43 @@
+console.log("We are in tut11");
+let l = window;
+// console.log(l);
+// alert("Good Morning,Arun")
+// window.alert("Have a nice day!")
+
+
+// p = prompt("This will destroy your pc")
+// console.log(p);
+
+
+// c = confirm("Press Ok to store true , else cancel for false a")
+// console.log(c);
+
+// let d = window.document;
+// console.log(d);
+
+
+// h = window.innerHeight; 
+// d= window.innerWidth
+// console.log(h);
+// console.log(d);
+
+
+// let x = scrollX
+// let y = scrollY
+// console.log(x);
+// console.log(y);
+
+l = location;
+console.log(l);
+// location.reload(); //use in console
+console.log(location.href);
+
+// location.href = "https://google.com";
+
+// let s = location.toString();
+// console.log(s);
+
+h = window.history;
+console.log(h);
+// history.go(-1);//try in console
+// history.go(1);//try in console
