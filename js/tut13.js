5f7a5bd4218478cec7bb2c0a4015a9722b4cb49d
Arun4535 <arunkhanchandani05@gmail.com>
2021-08-17 16:47:40 +0530

    Exercise 1

--- js/tut13.js
+++ js/tut13.js
@@ -1,6 +1,6 @@
-links = document.links;
+links = document.links; //all links on the webpage
 // console.log(links);
-link = Array.from(links);
+link = Array.from(links); //converting into array, and checking each value if it contains youtube link
 // console.log(link);
 let href;
 Array.from(links).forEach(function(element){
