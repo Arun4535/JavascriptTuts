98afd6befa6d8e4a2f6edda0a5f0dfd43cb6a0b9
Arun4535 <arunkhanchandani05@gmail.com>
2021-08-18 21:27:28 +0530

    creating an editable div

--- js/tut21.js
+++ js/tut21.js
@@ -2,8 +2,8 @@ let divElement = document.createElement('div')
 let text = document.createTextNode('Edit me.....')
 divElement.appendChild(text);
 
-divElement.setAttribute('id','elem');
-divElement.setAttribute('class','elem')
+divElement.setAttribute('id', 'elem');
+divElement.setAttribute('class', 'elem')
 divElement.setAttribute('style', 'border:2px solid red;width:150px;margin:20px;padding:20px')
 
 let container = document.querySelector('.container');
@@ -11,10 +11,24 @@ let first = document.querySelector('#myfirst')
 
 
 //Insert element before first
-container.insertBefore(divElement,first);
+container.insertBefore(divElement, first);
 
-divElement.addEventListener('click', function(){
-    
+divElement.addEventListener('click', function () {
+
+    let numTextArea = document.getElementsByClassName('textArea').length;
+    console.log(elem);
+    if (numTextArea == 0) {
+        let html = elem.innerHTML;
+        divElement.innerHTML = `<textarea class="form-control textArea" id="textarea" rows="3"> ${html} </textarea>`
+    }
+
+
+    //blur event
+    let textArea = document.getElementById('textArea');
+    textArea = document.addEventListener('blur', function () {
+        elem.innerHTML = textArea;
+    })
 })
+
 
 // console.log(divElement,container,first);
