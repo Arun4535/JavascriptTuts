c1329a4b732ba9381771b70d5711e38073a69d49
Arun4535 <arunkhanchandani05@gmail.com>
2021-08-24 18:31:58 +0530

    arrow function

---
+++ js/tut41.js
@@ -0,0 +1,37 @@
+console.log('Link check');
+
+console.log('this is tutorial 41');
+
+// ARROW FUNCTIONS
+
+// Creating a regular function
+// const harry = function (){
+//     console.log("This is the best person ever")
+// }
+
+// Converting it to an arrow function
+// const harry = ()=>{
+//     console.log("This is the best person ever")
+// }
+// harry();
+
+// function returning something
+const greet = function(){
+    return "Good Morning";
+}
+console.log(greet());
+
+
+// One liners do not require braces/return
+// one line will automatically return
+// const greet = () =>  "Good Morning";
+
+//returning object
+// const greet = () =>  ({name: "harry"});
+
+// Single parameters do not need parenthesis 
+// but you will have to put parenthesis if there are multiple paramteres
+// const greet = name =>  "Good Morning " + name + ending;
+
+
+// console.log(greet('Harry'))
